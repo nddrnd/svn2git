cea5c9d2f2db08181c3a3716f6f0ecc8503770bb
Yann Baud <yann.baud@ndd.ch>
2019-05-29 13:05:42 +0200

    entrypoint.sh: fix one issue when continuing previous work
    Workaround in entrypoint.sh deletes the .git directory of the converted SVN repos.
    It should use a dummy directory, otherwise a previously run svn2git
    command that stopped in the middle of a conversion cannot resume its work correctly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -ex
 # svn2git ask for username/password once to access the SVN repository, but does not deal correctly
 # with user input. So use "svn init" to set the username/password prior running svn2git.
 # Source: https://github.com/nirvdrum/svn2git/issues/59
-git svn init --prefix=svn/ --trunk='trunk' svn://nddsrv10/software/Projects/PC/SpiroCoreDotNetWrapper && rm -rf .git && svn2git "$@"
+git svn init --prefix=svn/ --trunk='trunk' svn://nddsrv10/software/Projects/PC/SpiroCoreDotNetWrapper .dummy-dir && rm -rf .dummy-dir && svn2git "$@"
